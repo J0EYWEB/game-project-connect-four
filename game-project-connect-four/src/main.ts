499c16e968a1e6ebd6615c63910e2da2c1cfc019
Josef Webster <joeywebster97@gmail.com>
2024-08-28 14:49:10 +0100

    Created for loop which returns cell number when clicked

--- game-project-connect-four/src/main.ts
+++ game-project-connect-four/src/main.ts
@@ -9,13 +9,19 @@ if (!allCells){
 };
 
 
+for(let i: number | any = 0; i < allCells.length; i++ ){
+    allCells[i].addEventListener('click', (cell) =>{
+    console.log([i])});
+}
+
+
 // When cell is clicked, 
 const handleClickCells = (event: Event) => {
     const cell = event.currentTarget as HTMLDivElement;
     if(cell.classList.contains('filled')){
         alert('Cell has already been filled, please pick another one!');
     } else {
-        if(count % 2 === 0 || count === 0){
+        if(count % 2 === 0) {
             cell.style.backgroundColor = 'red';
             cell.classList.add('filled')
             count += 1;
