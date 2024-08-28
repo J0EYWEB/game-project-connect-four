de60e3097de1e6210dabf2fb901e8927186bb06e
Josef Webster <joeywebster97@gmail.com>
2024-08-28 12:33:37 +0100

    Created counter that increments with each click on cells changing colour from red to yellow. Also created alert if cell already filled.

--- game-project-connect-four/src/main.ts
+++ game-project-connect-four/src/main.ts
@@ -2,15 +2,32 @@ import './style.css'
 
 const allCells = document.querySelectorAll<HTMLDivElement>('.game-board__cell');
 
-
+let count: number = 0;
 
 if (!allCells){
     throw new Error("Some element was not found");
 };
 
+
+// When cell is clicked, 
 const handleClickCells = (event: Event) => {
     const cell = event.currentTarget as HTMLDivElement;
-    cell.style.backgroundColor = 'red';
+    if(cell.classList.contains('filled')){
+        alert('Cell has already been filled, please pick another one!');
+    } else {
+        if(count % 2 === 0 || count === 0){
+            cell.style.backgroundColor = 'red';
+            cell.classList.add('filled')
+            count += 1;
+        } else {
+            cell.style.backgroundColor = 'yellow';
+            cell.classList.add('filled')
+            count +=1;
+        }
+    }
+
+    
+    
 };
 
 allCells.forEach((cell) => {
