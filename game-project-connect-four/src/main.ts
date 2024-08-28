7ae57271f9fca35d08805d637ddeee1af3f05e6a
Josef Webster <joeywebster97@gmail.com>
2024-08-28 12:01:20 +0100

    Created event handler and listener to colour cells red when clicked

--- game-project-connect-four/src/main.ts
+++ game-project-connect-four/src/main.ts
@@ -1,2 +1,18 @@
 import './style.css'
 
+const allCells = document.querySelectorAll<HTMLDivElement>('.game-board__cell');
+
+
+
+if (!allCells){
+    throw new Error("Some element was not found");
+};
+
+const handleClickCells = (event: Event) => {
+    const cell = event.currentTarget as HTMLDivElement;
+    cell.style.backgroundColor = 'red';
+};
+
+allCells.forEach((cell) => {
+    cell.addEventListener('click', handleClickCells);
+});
