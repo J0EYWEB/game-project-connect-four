e4a00f35d9e58ef74d580fae29ac1d9304b93f56
Josef Webster <joeywebster97@gmail.com>
2024-08-29 20:22:52 +0100

    Added styling to game board. Box shadow changes colour depending on player turn

--- game-project-connect-four/src/main.ts
+++ game-project-connect-four/src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
 //Query Selectors
+const gameBoard = document.querySelector<HTMLDivElement>('.game-board');
 const allCells = document.querySelectorAll<HTMLDivElement>('.game-board__cell');
 const playerTurn = document.querySelector<HTMLSpanElement>('.player-turn');
 
 //Variables
 let count: number = 0;
 
-if (!allCells || !playerTurn){
+if (!allCells || !playerTurn || !gameBoard){
     throw new Error("Some element was not found");
 };
 
@@ -23,12 +24,14 @@ for(let i: number = 0; i < allCells.length; i++ ){
                     allCells[i].classList.add('filled');
                     count += 1;
                     playerTurn.innerHTML = ' Player Two'
+                    gameBoard.style.boxShadow = '0 0 100px yellow';
 
                 } else if(count % 2 === 1){
                     allCells[i].style.backgroundColor = 'yellow';
                     allCells[i].classList.add('filled')
                     count +=1;
                     playerTurn.innerHTML = ' Player One'
+                    gameBoard.style.boxShadow = '0 0 100px red';
                 } 
             }
         }
