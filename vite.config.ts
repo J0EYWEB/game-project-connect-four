df1907283cf3d79815d94fa5529870b162cbc16c
J0EYWEB <139481554+J0EYWEB@users.noreply.github.com>
2024-09-03 17:03:13 +0100

    Update vite.config.ts
    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/game-project-connect-four/',
+  base: "/game-project-connect-four/",
 });
